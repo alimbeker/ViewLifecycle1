84ea99df56b4ac0a6f6eed93f75c21fded521979
Amir Alimbekerov <amiralimbekerov@Amirs-MacBook-Air.local>
2023-11-14 23:08:26 +0600

    changes in zoom

--- app/src/main/java/com/example/viewlifecycle/ToolbarView.kt
+++ app/src/main/java/com/example/viewlifecycle/ToolbarView.kt
@@ -1,8 +1,10 @@
 package com.example.viewlifecycle
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import com.example.viewlifecycle.databinding.ViewToolbarBinding
 
@@ -12,6 +14,26 @@ class ToolbarView @JvmOverloads constructor(
 
     private val binding: ViewToolbarBinding =
         ViewToolbarBinding.inflate(LayoutInflater.from(context), this)
+     init {
+         setAttrs(attrs,R.styleable.ToolbarView){
+             binding.temp.text = "${it.getInt(R.styleable.ToolbarView_temp,10)}"
 
+         }
+     }
+}
+
+inline fun View.setAttrs(
+    attrs: AttributeSet?,
+    styleable: IntArray,
+    crossinline body: (TypedArray) -> Unit
+) {
+    context.theme.obtainStyledAttributes(attrs, styleable, 0, 0)
+        .apply {
+            try {
+                body.invoke(this)
+            } finally {
+                recycle()
+            }
+        }
 }
 
